3e67e1bdf66d2f1bbc325d68d18f574dca4b443d
chrissi9290 <107833220+chrissi9290@users.noreply.github.com>
2025-03-14 20:50:08 +0100

    vite.config.js aktualisieren

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/SolSniperBot-new/', // <- Hier muss der Name deines GitHub Repos stehen!
   server: {
     port: 3000
   }
